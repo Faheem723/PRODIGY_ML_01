030edb2728e3fd26d28bf668086425ac967f58dc
Faheem723 <131781715+Faheem723@users.noreply.github.com>
2023-12-07 00:21:57 +0530

    Update Machine Learning Task_One-Linear Regression.py

--- Machine Learning Task_One-Linear Regression.py
+++ Machine Learning Task_One-Linear Regression.py
@@ -8,7 +8,6 @@ TASK-1:
 import pandas as pd
 import numpy as np
 import seaborn as sb
-import math
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
